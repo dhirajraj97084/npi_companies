c2e39d44c9180bc3758da647a90f82ed32a067fb
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-08-07 11:23:14 +0530

    nice

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Home from './pages/home/Home';
 import AboutUs from './pages/aboutUs/AboutUs';
 import ContactUs from './pages/contactUs/ContactUs';
 import NoPage from './pages/noPage/NoPage';
+import ScrollTop from './component/scrollTop/ScrollTop';
 
 function App() {
   return (
     <div>
      <Router>
+      <ScrollTop/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/aboutUs' element={<AboutUs/>}/>
